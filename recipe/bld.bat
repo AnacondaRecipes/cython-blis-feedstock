dc614d0d28756d62592764fd52d823c7a79e2be6
Kai Tietz <ktietz@anaconda.com>
2022-02-04 22:49:58 +0100

    attempt to add 32-bit support

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,11 @@
 set "BLIS_COMPILER="
+
+if %ARCH% == 64 (
+  set "CC=clang-cl.exe"
+  set "CXX=clang-cl.exe"
+) else (
+  set "CC=clang-cl.exe -m32"
+  set "CXX=clang-cl.exe -m32"
+)
+
 %PYTHON% -m pip install . --no-deps -vvv
